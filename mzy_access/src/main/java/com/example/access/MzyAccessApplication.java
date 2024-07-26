0dc3bf61ceb2358874a398e196093a975d88687f
mzy <472413352@qq.com>
2024-07-27 02:10:25 +0800

    初始化

---
+++ mzy_access/src/main/java/com/example/access/MzyAccessApplication.java
@@ -0,0 +1,13 @@
+package com.example.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzyAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MzyAccessApplication.class, args);
+    }
+
+}
